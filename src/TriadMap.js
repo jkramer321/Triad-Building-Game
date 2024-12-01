03323ea21d65ef82fa32c7c482bad67aa8ba1a02
Jared Kramer <jmkramer5@wisc.edu>
2024-12-01 12:24:27 -0500

    Create TriadMap.js

---
+++ src/TriadMap.js
@@ -0,0 +1,27 @@
+//creating a map of triads to their respective notes 
+const triadMap = {
+    // C Triads
+    "C Major": ["C", "E", "G"],
+    "C Minor": ["C", "D#", "G"],
+    "C Diminished": ["C", "D#", "F#"],
+    "C Augmented": ["C","E","G#"],
+
+    // C# Triads
+    "C# Major":["C#", "E#", "G#"],
+    "C# Minor":["C#","E","G#"],
+    "C# Diminished":["C#", "E","G"],
+    "C# Augmented":["C#","E#","G##"],
+
+    // D Triads
+    "D Major":["D", "F#", "A"],
+    "D Minor":["D","F","A"],
+    "D Diminished":["D","F","Ab"],
+    "D Augmented":["D","F#","A#"],
+    
+
+
+
+};
+
+export default triadMap;
+
